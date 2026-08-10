e61762070a146454d957dc085eaecf0e8ccec325
Repon Hossain <repon.kushtia@gmail.com>
2026-08-10 12:39:10 +0600

    ddd

--- inc/conditions/class-cart-products.php
+++ inc/conditions/class-cart-products.php
@@ -73,43 +73,51 @@ final class Cart_Products {
 	 * @since 1.0.0
 	 * @return boolean
 	 */
-	public function validate_condition($matched, $condition) {
-		$cart_total = new Cart_Total();
-		$cart_items = $cart_total->get_cart_items();
-
-		$model_key = null;
-		$current_cart_values = array();
-		foreach (Utils::get_product_taxonomies() as $tax_key => $taxonomy) {
-			if ('cart_products:' . $tax_key === $condition['type']) {
-				$model_key = 'cart_products_' . $taxonomy['model'];
-				$current_cart_values = $cart_total->get_terms($tax_key);
-				break;
-			}
-		}
-
-		if (!isset($condition[$model_key]) || !is_array($condition[$model_key])) {
+	public function validate_condition($condition) {
+		$model_values = $condition->get_value($condition->get_model_key());
+		if (empty($model_values) || !is_array($model_values)) {
 			return false;
 		}
 
-		$cart_products_operator = 'any_in_list';
-		if (!empty($condition['cart_products_operator'])) {
-			$cart_products_operator = $condition['cart_products_operator'];
+		$data_type = $condition->get_data_type();
+		if (empty($data_type)) {
+			$data_type = '';
 		}
 
-		$matched_items = array_intersect($current_cart_values, $condition[$model_key]);
-		if ('any_in_list' == $cart_products_operator) {
-			return count($matched_items) > 0;
+		$object_info = explode(':', $data_type);
+		if (!isset($object_info[1])) {
+			$object_info[1] = '';
 		}
 
-		if ('all_in_list' == $cart_products_operator) {
-			return count($condition[$model_key]) === count($matched_items);
+		$cart_total = new Cart_Total();
+
+		$cart_object_ids = array();
+		if ('taxonomy' == $object_info[0]) {
+			$cart_object_ids = $cart_total->get_terms($object_info[1]);
 		}
 
-		if ('not_in_list' == $cart_products_operator) {
-			return count($matched_items) === 0;
+		$cart_object_ids = apply_filters(
+			Utils::get_hook_name('condition', 'cart-products', 'validate-condition', 'cart-object-ids'),
+			$cart_object_ids,
+			$cart_total
+		);
+
+		$matched_object_ids = array_intersect($model_values, $cart_object_ids);
+
+		$operator = $condition->get_value('cart_products_operator');
+		if ('any_in_list' == $operator) {
+			return count($matched_object_ids) > 0;
 		}
 
-		return $matched;
+		if ('all_in_list' == $operator) {
+			return count($model_values) === count($matched_object_ids);
+		}
+
+		if ('not_in_list' == $operator) {
+			return count($matched_object_ids) === 0;
+		}
+
+		return false;
 	}
 
 	/**
